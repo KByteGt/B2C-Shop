a410182aed716ba4961ceb108d9d44169ddadb48
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-01-28 20:16:34 -0600

    Adding Bootstrap in React

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,25 @@
+import {Jumbotron, Button, Container } from 'react-bootstrap';
+import style from './Home.module.css'
 const Home = (props) => {
-    return(<h1>Home page</h1>)
+    return(
+        <>
+        <div className={style.spaceNavbar}></div>
+        <Jumbotron>
+            <Container>
+                <h1>Hello, world!</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac velit tortor. Integer non lorem sed nunc congue feugiat et eleifend tortor. Ut vulputate urna mi, et accumsan purus placerat id. 
+                </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Container>
+        </Jumbotron>
+        <Container>
+            <h1>Most Popular Products</h1>
+        </Container>
+        </>
+    )
 }
 
 export default Home;
--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,14 +1,48 @@
 import style from './ToolBar.module.css';
 import NavItems from '../NavItems/NavItems';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button'
+import { Container } from 'react-bootstrap';
 
-const ToolBar = (props) => {
-    return(
-        <div className={style.ToolBar}>
+
+function ToolBar(props) {
+    return (
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="/">Moon Dreams</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/about">About Us</Nav.Link>
+                        <Nav.Link href="/catalogue">Catalogue</Nav.Link>
+                        <Nav.Link href="/contact">Contact Us</Nav.Link>
+                        {/*
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        */}
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-dark">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        /*<div className={style.ToolBar}>
             <nav>
                 <h3>Logo</h3>
-                <NavItems/>
+                <NavItems />
             </nav>
-        </div>
+    </div>*/
     );
 }
 
