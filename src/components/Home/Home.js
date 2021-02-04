5a07732a5b8f83df307cc2d8af5b94b53f822976
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-02-04 01:28:31 -0600

    Adding style to Hero section in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,17 @@ import style from './Home.module.css'
 const Home = (props) => {
     return(
         <>
-        <Jumbotron>
+        <Jumbotron className={style.bg_hero}>
             <Container>
-                <h1>Hello, Fortniter!</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac velit tortor. Integer non lorem sed nunc congue feugiat et eleifend tortor. Ut vulputate urna mi, et accumsan purus placerat id. 
-                </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
-                </p>
+                <div className={style.hero_content}>
+                    <h1>Hello, Fortniter!</h1>
+                    <p >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac velit tortor. Integer non lorem sed nunc congue feugiat et eleifend tortor. Ut vulputate urna mi, et accumsan purus placerat id. 
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </div>
             </Container>
         </Jumbotron>
         <Container>
