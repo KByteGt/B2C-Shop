25fa078ac124fb5866bb01a39702d67639be37b2
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-02-09 00:20:25 -0600

    Refactoring NavBar.js

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -1,7 +1,5 @@
-import style from './NavBar.module.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button'
